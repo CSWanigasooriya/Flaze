85b68d2dafe2689cae90d8694003de2728564528
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2021-09-05 08:16:11 +0530

    Login Screen

--- app/src/main/java/com/flaze/tracer/MainActivity.kt
+++ app/src/main/java/com/flaze/tracer/MainActivity.kt
@@ -5,23 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import com.flaze.tracer.ui.login.LoginScreen
 import com.flaze.tracer.ui.theme.FlazeTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
             FlazeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     color = MaterialTheme.colors.background,
                 ) {
-
+                    LoginScreen()
                 }
             }
         }
     }
 }
+
 
